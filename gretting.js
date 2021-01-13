71263a4ba6883ab8d00d6ffadde6aea388f59273
dasol-jeong <huiogc1214@gmail.com>
2021-01-13 19:00:40 +0900

    used weather API

--- gretting.js
+++ gretting.js
@@ -1,26 +1,44 @@
 const form=document.querySelector(".js-form"),
-        input=form.querySelector("input"),
-        greeting=document.querySelector(".js-greetings");
+input=form.querySelector("input"),
+greeting=document.querySelector(".js-greetings");
 
 const USER_LS="currentUser",
-SHOWING_CN="showing"
+SHOWING_CN="showing";
 
-function paintGreeting(text){
+function saveName(text){
+    localStorage.setItem(USER_LS,text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue=input.value;
+    paintName(currentValue);
+    saveName(currentValue);
+}
+
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit",handleSubmit);
+}
+function paintName(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText=`Hello &{text}`;
+    greeting.innerText=`Hello ${text}`;
 }
+
+
 function loadName(){
-    const cuurentUser=localStorage.getItem(USER_LS);
-    if(currentUser===null){
-        //user is not
-        
-    }else{
-        paintGreeting(cuurentUser);
-        //user is
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null){
+        askForName();
+    }
+    else{
+        paintName(currentUser);
     }
 }
-function init(){
 
+function init(){
+    loadName();
 }
 init();
